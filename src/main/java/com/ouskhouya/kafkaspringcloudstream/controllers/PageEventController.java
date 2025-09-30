c7bf45df08a1df2fb9c75a469be8ea5b742f4f0e
Oussama khouya <ouskhouya@gmail.com>
2025-09-30 18:38:03 +0100

    supplier Kafka envoie un message chaque 200ms

--- src/main/java/com/ouskhouya/kafkaspringcloudstream/controllers/PageEventController.java
+++ src/main/java/com/ouskhouya/kafkaspringcloudstream/controllers/PageEventController.java
@@ -1,13 +1,16 @@
 package com.ouskhouya.kafkaspringcloudstream.controllers;
 
 import com.ouskhouya.kafkaspringcloudstream.events.PageEvent;
+import org.apache.kafka.streams.processor.internals.RecordCollector;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 import java.util.Random;
+import java.util.function.Supplier;
 
 @RestController
 public class PageEventController {
@@ -28,5 +31,15 @@ public class PageEventController {
                 new Date(), 10 + new Random().nextInt(1000));
         streamBridge.send(topic, event);
         return event;
+    }
+
+    @Bean
+    public Supplier<PageEvent> pageEventSupplier(){
+        return ()-> new PageEvent(
+                Math.random()>0.5?"P1":"P2",
+                Math.random()>0.5?"U1":"U2",
+                new Date(),
+                10+new Random().nextInt(10000)
+        );
     }
 }
